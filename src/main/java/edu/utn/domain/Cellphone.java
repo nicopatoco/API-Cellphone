21d5034461f2427c4ba0f1dd00cfe74542edb0ae
Francisco Franco <franciscoleandrofranco@gmail.com>
2020-04-25 18:56:48 -0300

    initial commit

---
+++ src/main/java/edu/utn/domain/Cellphone.java
@@ -0,0 +1,12 @@
+package edu.utn.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cellphone {
+}
---
+++ src/test/java/edu/utn/test/Test.java
@@ -0,0 +1,10 @@
+package edu.utn.test;
+
+import edu.utn.domain.Cellphone;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println("hola");
+    }
+}
