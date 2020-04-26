caf0be8c1fd3ba096cf520074bf6b1e5c99b2728
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-04-26 17:50:53 -0300

    add tp

--- src/test/java/edu/utn/test/TestConnection.java
+++ src/test/java/edu/utn/test/TestConnection.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestConnection {
-
     @Test
     public void checkConnection() {
         System.out.println("checking connection");
         Assertions.assertTrue(true);
     }
-
-
 }
