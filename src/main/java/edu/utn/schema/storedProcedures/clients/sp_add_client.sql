265ac76f4080e9570c4f52f2c680c24424367ec9
Francisco Franco <franciscoleandrofranco@gmail.com>
2020-04-27 19:45:29 -0300

    Fix in stored procedure

--- src/main/java/edu/utn/schema/storedProcedures/clients/sp_add_client.sql
+++ src/main/java/edu/utn/schema/storedProcedures/clients/sp_add_client.sql
@@ -4,7 +4,7 @@ DELIMITER //
 CREATE PROCEDURE ADD_CLIENT (IN cId varchar(50), IN cName varchar(50), IN cSurname varchar(50), In cCityId int, OUT newId int)
 BEGIN
 	IF ((select count(id) from Clients where id = cId) = 0) then 
-		insert into Clients (id, name, surname, city_id) values (cId, cName, cSurname, cCityId);
+		insert into Clients (id, name, surname, id_city) values (cId, cName, cSurname, cCityId);
 		set newId = LAST_INSERT_ID();
 	ELSE
 		signal sqlstate '10001' 
