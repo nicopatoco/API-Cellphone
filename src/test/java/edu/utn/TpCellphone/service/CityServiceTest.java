1abd49da8256e0f52d4011609643443e611a2b71
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-05-21 18:57:58 -0300

    Update CityServiceTest.java
    add province and city service tests

--- src/test/java/edu/utn/TpCellphone/service/CityServiceTest.java
+++ src/test/java/edu/utn/TpCellphone/service/CityServiceTest.java
@@ -1,8 +1,8 @@
 package edu.utn.TpCellphone.service;
 
-
 import edu.utn.TpCellphone.model.Cities;
 import edu.utn.TpCellphone.repository.CityRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,54 +23,67 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.WARN)
 public class CityServiceTest {
-
     @InjectMocks
     private CityService cityService;
-
+    
     @Mock
     private CityRepository repository;
-
-    private Cities cities;
-
+    
+    private Cities city;
+    
     @BeforeEach
     public void setUo() {
-        this.cities = new Cities(1, 1, "mar del plata", 223);
+        this.city = new Cities(1, "mar del plata", 223, 1, null);
     }
-
+    
     @Test
     public void getByIdTest() {
-        when(repository.findById(1)).thenReturn(java.util.Optional.of(cities));
+        when(repository.findById(1)).thenReturn(java.util.Optional.of(city));
         Optional<Cities> response = cityService.getById(1);
-
+        
         assertNotNull(response);
-        assertEquals(1, cities.getId_city());
+        assertEquals(1, city.getId_city());
     }
-
+    
     @Test
     public void addClientTest() {
-        when(repository.save(cities)).thenReturn(cities);
-        cityService.addClient(cities);
-
-        verify(repository, times(1)).save(cities);
+        when(repository.save(city)).thenReturn(city);
+        Cities response = cityService.addCity(city);
+        
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(city, response);
     }
-
+    
     @Test
     public void getAllTest() {
         List<Cities> citiesList = new ArrayList<>();
-        citiesList.add(cities);
+        citiesList.add(city);
         when(repository.findAll()).thenReturn(citiesList);
         List<Cities> response = cityService.getAll();
-
+        
         assertNotNull(response);
         assertEquals(citiesList, response);
     }
-
+    
     @Test
     public void deleteTest() {
-        doNothing().when(repository).delete(cities);
-        cityService.delete(cities);
-
-        verify(repository, times(1)).delete(cities);
+        doNothing().when(repository).delete(city);
+        cityService.delete(city);
+        
+        verify(repository, times(1)).delete(city);
     }
-
+    
+    @Test
+    public void updateTest() {
+        Cities cityToUpdate = new Cities();
+        cityToUpdate.setName("Pinamar");
+        cityToUpdate.setPrefix(225);
+        cityToUpdate.setId_province(1);
+        int idToUpdate = 1;
+        when(repository.getOne(idToUpdate)).thenReturn(city);
+        when(repository.save(city)).thenReturn(city);
+        Cities response = cityService.update(cityToUpdate, idToUpdate);
+        
+        Assertions.assertEquals(response ,city);
+    }
 }
