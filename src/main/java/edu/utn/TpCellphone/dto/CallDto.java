1d17e71cbdd4e8a157098b587a2cc42f2d208141
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-06-16 10:11:22 -0300

    add call dto

--- src/main/java/edu/utn/TpCellphone/dto/CallDto.java
+++ src/main/java/edu/utn/TpCellphone/dto/CallDto.java
@@ -5,19 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CallDto {
-    
     @JsonProperty
-    private Date startTime;
+    private String numberOrigin;
     @JsonProperty
-    private Date endTime;
+    private String numberDestination;
     @JsonProperty
-    private String number_origin;
-    @JsonProperty
-    private String number_destination;
+    private int duration;
 }
