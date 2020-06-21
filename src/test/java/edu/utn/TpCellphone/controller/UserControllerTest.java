d694c1f6a04376ec827809eb9e3daef1842c8b5d
jdromerorajoy <jd.romerorajoy@gmail.com>
2020-06-20 22:48:56 -0300

    Update UserControllerTest.java

--- src/test/java/edu/utn/TpCellphone/controller/UserControllerTest.java
+++ src/test/java/edu/utn/TpCellphone/controller/UserControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
