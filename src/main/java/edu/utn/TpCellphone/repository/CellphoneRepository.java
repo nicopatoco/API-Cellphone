879ce071933d0cd566ce7270aa04c69560b4375d
jdromerorajoy <jd.romerorajoy@gmail.com>
2020-06-23 19:23:09 -0300

    Update CellphoneRepository.java

--- src/main/java/edu/utn/TpCellphone/repository/CellphoneRepository.java
+++ src/main/java/edu/utn/TpCellphone/repository/CellphoneRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface CellphoneRepository extends JpaRepository<Cellphone, Integer> {
     
-    @Query(value = "select * from cellphones where cellphone_number = ? and status = 1")
+    @Query(value = "select * from cellphones where cellphone_number = ?1 and status = 1", nativeQuery = true)
     Optional<Cellphone> isAvailable(String cellphone);
 }
